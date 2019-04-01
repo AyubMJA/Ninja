acfe507f1739599c1178b41fde4701013d3d5869
Ayub <ayubali9996@gmail.com>
2019-04-01 18:50:12 -0400

    init push

---
+++ Ninja/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Ninja
+//
+//  Created by Ayub Ali on 2019-04-01.
+//  Copyright © 2019 Ayub Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+}
+
